c17f6eded2d2c9ccfc143fa1b648c0615b2a0056
Tomasz Wysocki <tomasz@wysocki.info>
2011-06-18 11:43:39 +0200

    Add version checking for .deb packages.

--- install_packages.sh
+++ install_packages.sh
@@ -19,8 +19,18 @@ for script in $(find install_packages -name '*.sh'); do ./$script; done
 
 # install from .deb files
 function install_from_deb {
+        echo "Installing" $1
         PACKAGE=$(dpkg --info $1  | grep Package | awk '{ print $2 }')
-        if [ -n "$((dpkg --status $PACKAGE || true) | grep 'Status: install ok installed')" ]; then return; fi
+        VERSION=$(dpkg --info $1  | grep Version | awk '{ print $2 }')
+        if [ -n "$((dpkg --status $PACKAGE || true) | grep 'Status: install ok installed')" ]; then 
+            echo -n "Already installed. Checking version... ";
+            if [ $VERSION == $(dpkg --status $PACKAGE  | grep Version | awk '{ print $2 }') ]; then
+                echo "match."
+                return;
+            else
+                echo "not match."
+            fi
+        fi
 	sudo gdebi -n $1
 }
 for deb in $(find install_packages -name '*.deb'); do install_from_deb ./$deb; done
